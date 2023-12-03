fb0d3cfbb832b8772f3a0170516a9a1926829035
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-12-03 21:43:48 +0200

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -11,8 +11,8 @@ const projectsData = [
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
-    gitUrl: "#",
-    previewUrl: "#",
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 2,
