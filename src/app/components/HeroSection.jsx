4633c88e4af8651320629e75a54e1976092186e6
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-12-09 11:34:47 +0200

    fix image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -64,7 +64,7 @@ const HeroSection = () => {
         >
           <div className=" bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/DSC_5346.jpg"
+              src="/images/me.png"
               alt="Abd elhakim image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={400}
